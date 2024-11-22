9f01b33431bbd7f1194d08b1fa59ee76cb84304d
Elely <zianiabdoullah@gmail.com>
2024-11-22 16:22:44 +0100

    Changement sur App

--- src/App.jsx
+++ src/App.jsx
@@ -13,14 +13,11 @@ export function App() {
     <Provider store={store}>
       <BrowserRouter basename="/tpreact1">
         <Navbar />
-        {/* Tout ce qui est ici, c'est le contenu de mon router */}
         <Routes>
-          {/* Ici, il va y avoir chacune des routes individuelles */}
           <Route path="/" Component={Accueil} />
           <Route path="/filmlist" Component={FilmList} />
           <Route path="/film/:id" Component={FilmDetails} />
           <Route path="/Neco" Component={Neco} />
-          {/* Pour une page 404, un path * pour indiquer un wildcard sur toutes les autres url non spécifiées au dessus*/}
           <Route path="*" Component={NotFound} />
         </Routes>
       </BrowserRouter>
